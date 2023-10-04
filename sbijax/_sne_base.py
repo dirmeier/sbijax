01f2e2c988360d82d895be8cea35069fdf81138b
Simon Dirmeier <dirmeier@users.noreply.github.com>
2023-10-04 14:14:45 +0200

    Fix (#15)
    * Add lower-level simulate functions

--- sbijax/_sne_base.py
+++ sbijax/_sne_base.py
@@ -59,8 +59,55 @@ class SNE(SBI, ABC):
         """
 
         observable = jnp.atleast_2d(observable)
-        sample_key, rng_key = jr.split(rng_key)
+        new_data, diagnostics = self.simulate_data(
+            rng_key,
+            params=params,
+            observable=observable,
+            n_simulations=n_simulations,
+            **kwargs,
+        )
         if data is None:
+            d_new = new_data
+        else:
+            d_new = self.stack_data(data, new_data)
+        return d_new, diagnostics
+
+    def simulate_data(
+        self,
+        rng_key,
+        *,
+        params=None,
+        observable=None,
+        n_simulations=1000,
+        **kwargs,
+    ):
+        """
+        Simulate data from the posterior or prior and append it to an
+        existing data set (if provided)
+
+        Parameters
+        ----------
+        rng_key: jax.PRNGKey
+            a random key
+        params: Optional[pytree]
+            a dictionary of neural network parameters. If None, will draw from
+            prior. If parameters given, will draw from amortized posterior
+            using 'observable;
+        observable: Optional[jnp.ndarray]
+            an observation. Needs to be gfiven if posterior draws are desired
+        n_simulations: int
+            number of newly simulated data
+        kwargs: keyword arguments
+            dictionary of ey value pairs passed to `sample_posterior`
+
+        Returns
+        -------
+        NamedTuple:
+            returns a NamedTuple of two axis, y and theta
+        """
+
+        sample_key, rng_key = jr.split(rng_key)
+        if params is None or len(params) == 0:
             diagnostics = None
             self.n_total_simulations += n_simulations
             new_thetas = self.prior_sampler_fn(
@@ -68,12 +115,17 @@ class SNE(SBI, ABC):
                 sample_shape=(n_simulations,),
             )
         else:
+            if observable is None:
+                raise ValueError(
+                    "need to have access to 'observable' "
+                    "when sampling from posterior"
+                )
             if "n_samples" not in kwargs:
                 kwargs["n_samples"] = n_simulations
             new_thetas, diagnostics = self.sample_posterior(
                 rng_key=sample_key,
                 params=params,
-                observable=observable,
+                observable=jnp.atleast_2d(observable),
                 **kwargs,
             )
             perm_key, rng_key = jr.split(rng_key)
@@ -82,18 +134,33 @@ class SNE(SBI, ABC):
 
         simulate_key, rng_key = jr.split(rng_key)
         new_obs = self.simulator_fn(seed=simulate_key, theta=new_thetas)
+        chex.assert_shape(new_thetas, [n_simulations, None])
+        chex.assert_shape(new_obs, [n_simulations, None])
+
         new_data = named_dataset(new_obs, new_thetas)
 
-        chex.assert_shape(new_thetas, [n_simulations, None])
-        chex.assert_shape(new_data, [n_simulations, None])
+        return new_data, diagnostics
 
-        if data is None:
-            d_new = new_data
-        else:
-            d_new = named_dataset(
-                *[jnp.vstack([a, b]) for a, b in zip(data, new_data)]
-            )
-        return d_new, diagnostics
+    @staticmethod
+    def stack_data(data, also_data):
+        """
+        Stack two data sets.
+
+        Parameters
+        ----------
+        data: NamedTuple
+            one data set
+        also_data: : NamedTuple
+
+        Returns
+        -------
+        NamedTuple:
+            returns the stack of the two data sets
+        """
+
+        return named_dataset(
+            *[jnp.vstack([a, b]) for a, b in zip(data, also_data)]
+        )
 
     def as_iterators(
         self, rng_key, data, batch_size, percentage_data_as_validation_set
