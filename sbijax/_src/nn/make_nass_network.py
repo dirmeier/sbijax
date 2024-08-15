d73960f8ae8607400f05f27fc46a029cfc960cf0
Simon Dirmeier <simd23@protonmail.com>
2024-08-15 13:06:05 +0200

    FInal version with bug fixes

--- sbijax/_src/nn/make_nass_network.py
+++ sbijax/_src/nn/make_nass_network.py
@@ -9,33 +9,31 @@ from sbijax._src.nn.nasss_net import NASSSNet
 
 
 def make_nass_net(
-    summary_net_dimensions: Iterable[int],
-    critic_net_dimensions: Iterable[int],
+    embedding_dim: int,
+    hidden_sizes: Iterable[int],
     activation: Callable[[jax.Array], jax.Array] = jax.nn.relu,
 ):
     """Create a critic network for SNASS.
 
     Args:
-        summary_net_dimensions: a list of integers representing
-            the dimensionalities of the summary network. The _last_ dimension
-            determines the dimensionality of the summary statistic
-        critic_net_dimensions: a list of integers representing the
-            dimensionality of the critic network. The _last_ dimension
-            needs to be 1.
+        embedding_dim: dimensionality of the summary statistic
+        hidden_sizes: list of integers specifying the hidden dimensions
+            of the networks
         activation: a jax activation function
 
     Returns:
-        a network that can be used within a SNASS posterior estimator
+        a network that can be used within a NASS posterior estimator
     """
 
     @hk.without_apply_rng
     @hk.transform
     def _net(method, **kwargs):
         summary_net = hk.nets.MLP(
-            output_sizes=summary_net_dimensions, activation=activation
+            output_sizes=list(hidden_sizes) + [embedding_dim],
+            activation=activation,
         )
         critic_net = hk.nets.MLP(
-            output_sizes=critic_net_dimensions, activation=activation
+            output_sizes=list(hidden_sizes) + [1], activation=activation
         )
         net = NASSNet(summary_net=summary_net, critic_net=critic_net)
         return net(method, **kwargs)
